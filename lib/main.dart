794690262b3eea7d08b601d5a56f6a2a24294185
hagrasy <134404345+hagrasy@users.noreply.github.com>
2023-08-21 19:08:31 +0300

    before list view

--- lib/main.dart
+++ lib/main.dart
@@ -1,3 +1,4 @@
+import 'package:business_card_app/widgets/custom_button.dart';
 import 'package:flutter/material.dart';
 
 void main() {
@@ -10,14 +11,39 @@ class BusinessCardApp extends StatelessWidget {
   // This widget is the root of your application.
   @override
   Widget build(BuildContext context) {
-    return const MaterialApp(
+    return MaterialApp(
       debugShowCheckedModeBanner: false,
       home: Scaffold(
-        backgroundColor: Color(0xffd1d3d4),
+        backgroundColor: const Color(0xffd1d3d4),
         body: SafeArea(
-          child: Column(
-            children: [Image(image: AssetImage("images/4.png"))],
-          ),
+          child: Column(children: [
+            const Padding(
+              padding: EdgeInsets.symmetric(vertical: 30, horizontal: 60),
+              child: SizedBox(
+                width: 300,
+                height: 300,
+                child: CircleAvatar(
+                  backgroundColor: Colors.grey,
+                  radius: 250.0,
+                  child: CircleAvatar(
+                    backgroundImage: AssetImage('images/4.png'),
+                    radius: 200.0,
+                  ),
+                ),
+              ),
+            ),
+            const Text(
+              "Mohamed El Hagrasy",
+              style: TextStyle(
+                color: Color(0xff231f20),
+                fontSize: 35,
+                fontFamily: "fonts/OpenSans-ExtraBold.ttf",
+                fontWeight: FontWeight.bold,
+              ),
+            ),
+
+            // ListView.builder(itemBuilder: context.(context, index) => )
+          ]),
         ),
       ),
     );
